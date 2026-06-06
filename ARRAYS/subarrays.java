a47900726233843dd39d629d5190b7757032627c
Pavuluri Yoganand <yoganand8944@gmail.com>
2026-06-06 23:29:17 +0530

    Add subarrays class to print all subarrays

---
+++ ARRAYS/subarrays.java
@@ -0,0 +1,23 @@
+import java.util.*;
+public class subarrays{
+  public static void sub(int numbers[]){
+    int ts=0;
+    for(int i=0;i<numbers.length;i++){
+      int start=i;
+      for(int j=i;j<numbers.length;j++){
+        int end=j;
+        for(int k=start;k<=end;k++){
+          System.out.print(numbers[k]+" ");
+        }
+        ts++;
+        System.out.println();
+      }
+      System.out.println();
+    }
+    System.out.println("total subarrays:"+ts);
+  }
+    public static void main(String[] args){
+    int numbers[]={2,4,6,8,10};
+    sub(numbers);
+    }
+}  
