14c81cd69e87d9edbfc9c7d38a76d13b53bfad99
Pavuluri Yoganand <yoganand8944@gmail.com>
2026-06-07 22:49:40 +0530

    Optimize max subarray sum algorithm
    Optimized max subarray sum calculation using prefix sums.

---
+++ ARRAYS/maxsubarraysum2.java
@@ -0,0 +1,28 @@
+import java.util.*;
+public class maxsubarraysum{
+  public static void maxsubarrays(int numbers[]){
+    int currsum=0;
+    int maxsum=Integer.MIN_VALUE;
+    int prefix[]=new int[numbers.length];
+    prefix[0]=numbers[0];
+    for(int i=1;i<prefix.length;i++){
+      prefix[0]=prefix[i-1]+numbers[i];
+    }
+    for(int i=0;i<numbers.length;i++){
+      int start=i;
+      for(int j=i;j<numbers.length;j++){
+        int end=j;
+        currsum=start==0 ? prefix[end]:prefix[end]-prefix[start-1];
+        
+        if(maxsum<currsum){
+          maxsum=currsum;
+        }
+      }
+    }
+    System.out.println("max sum :" +maxsum);
+  }
+  public static void main(String[] args){
+    int numbers[]={1,-2,6,-1,3};
+    maxsubarrays(numbers);
+  }
+}// time complexity is O(n2) this is optimised code compared to other
