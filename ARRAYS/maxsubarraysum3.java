a5df65aa6702c8c70f0c2f83368e19827036ecd1
Pavuluri Yoganand <yoganand8944@gmail.com>
2026-06-08 23:00:45 +0530

    Optimize maxsubarrays method and add Kadane's algorithm
    Refactor maxsubarrays method and implement Kadane's algorithm for maximum subarray sum.

---
+++ ARRAYS/maxsubarraysum3.java
@@ -0,0 +1,39 @@
+import java.util.*;
+public class maxsubarraysum3{
+  public static void maxsubarrays(int numbers[]){
+    int currsum=0;
+    int maxsum=Integer.MIN_VALUE;
+    for(int i=0;i<numbers.length;i++){
+      int start=i;
+      for(int j=i;j<numbers.length;j++){
+        int end=j;
+        currsum=0;
+        for(int k=start;k<=end;k++){
+          currsum+=numbers[k];
+        }
+        System.out.println(currsum);
+        if(maxsum<currsum){
+          maxsum=currsum;
+        }
+      }
+    }
+    System.out.println("max sum :" +maxsum);
+  }
+  public static void kadanes(int numbers[]){
+    int ms=Integer.MIN_VALUE;
+    int cs=0;
+    for(int i=0;i<numbers.length;i++){
+      cs=cs+numbers[i];
+      if(cs<0){
+        cs=0;
+      }
+      ms=Math.max(cs,ms);
+  }
+  System.out.println("maximum sub aray sum:" +ms);
+  }
+    
+  public static void main(String[] args){
+    int numbers[]={-2,-3,4,-1,-2,1,5,-3};
+    kadanes(numbers);
+  }
+} // time complexity is O(n) very optimised code using kadanes algorithm
