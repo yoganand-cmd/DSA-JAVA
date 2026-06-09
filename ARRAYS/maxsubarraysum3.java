5e167ce67518e2227b6fbaf843d39db4f17f5ae5
Pavuluri Yoganand <yoganand8944@gmail.com>
2026-06-09 21:22:13 +0530

    updated optimised maxsubarraysum

--- ARRAYS/maxsubarraysum3.java
+++ ARRAYS/maxsubarraysum3.java
@@ -1,24 +1,5 @@
 import java.util.*;
 public class maxsubarraysum3{
-  public static void maxsubarrays(int numbers[]){
-    int currsum=0;
-    int maxsum=Integer.MIN_VALUE;
-    for(int i=0;i<numbers.length;i++){
-      int start=i;
-      for(int j=i;j<numbers.length;j++){
-        int end=j;
-        currsum=0;
-        for(int k=start;k<=end;k++){
-          currsum+=numbers[k];
-        }
-        System.out.println(currsum);
-        if(maxsum<currsum){
-          maxsum=currsum;
-        }
-      }
-    }
-    System.out.println("max sum :" +maxsum);
-  }
   public static void kadanes(int numbers[]){
     int ms=Integer.MIN_VALUE;
     int cs=0;
