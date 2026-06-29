fb17f5fb2e8f6491ad5d879c2d59dab154ab94be
Pavuluri Yoganand <yoganand8944@gmail.com>
2026-06-29 20:33:33 +0530

    Add bubble sort implementation in bubblesort.java
    Implement bubble sort algorithm and array input/output.

---
+++ sorting/bubblesort.java
@@ -0,0 +1,37 @@
+import java.util.*;
+public class bubblesort{
+  public static void bubble(int[] arr){
+    int swap=0;
+    for(int i=0;i<arr.length-1;i++){
+      for(int j=0;j<arr.length-1-i;j++){
+        if(arr[j]>arr[j+1]){
+          //swaping
+          int temp=arr[j];
+          arr[j]=arr[j+1];
+          arr[j+1]=temp;
+        }
+      }
+    }
+    System.out.println(+swap);
+  }
+  public static void printarr(int[] arr){
+    for(int i=0;i<arr.length;i++){
+      System.out.print(arr[i]+" ");
+    }
+    System.out.println();
+  }
+  public static void main(String[] args){
+    Scanner sc=new Scanner(System.in);
+    System.out.print("enter the size");
+    int n=sc.nextInt();
+    System.out.print("enetr array elements");
+    int[] arr=new int[n];
+    for(int i=0;i<n;i++){
+      arr[i]=sc.nextInt();
+    }
+    bubble(arr);
+    System.out.print("sorted array");
+    printarr(arr);
+  }
+}
+//time complexity is bad O(n2) here the swap shows the count of how many swaps had done 
