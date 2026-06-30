b0e72f51885e77136d4b56c2b4e15ca620df9c4e
Pavuluri Yoganand <yoganand8944@gmail.com>
2026-06-30 22:32:10 +0530

    Add insertion sort implementation in Java
    Implement insertion sort input handling in Java.

---
+++ sorting/insertionsort.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class insertionsort{
+  public static void main(String[] args){
+    Scanner sc=new Scanner(System.in);
+    System.out.print("Enter size :");
+    int n=sc.nextInt();
+    System.out.print("enter the elements :");
+    int[] arr=new int[n];
+    for(int i=0;i<n;i++){
+      arr[i]=sc.nextInt();
+    }
+  }
