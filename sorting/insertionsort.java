5892aa92295b615edc60d63b7d6e22e2666920fb
Pavuluri Yoganand <yoganand8944@gmail.com>
2026-07-18 23:11:15 +0530

    Add insertion sort implementation and print method
    Implemented insertion sort algorithm and added print function.

--- sorting/insertionsort.java
+++ sorting/insertionsort.java
@@ -1,5 +1,24 @@
 import java.util.*;
 public class insertionsort{
+public static void insertion(int arr[]){
+  for(int i=1;i<arr.length;i++){
+    int curr=arr[i];
+    int prev=i-1;
+    while(prev>=0&&arr[prev]>curr){
+      arr[prev+1]=arr[prev];
+      prev--;
+    }
+    arr[prev+1]=curr;
+  }
+
+   public static void printArr(int arr[]) {
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
+        System.out.println();
+   }
+    
+
   public static void main(String[] args){
     Scanner sc=new Scanner(System.in);
     System.out.print("Enter size :");
@@ -9,4 +28,8 @@ public class insertionsort{
     for(int i=0;i<n;i++){
       arr[i]=sc.nextInt();
     }
+    insertion(arr);
+    printArr(arr);
   }
+}
+//time complexity is O(n2)
