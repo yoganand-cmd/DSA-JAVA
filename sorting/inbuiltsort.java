63d979ce0e72e456aabe8ddeea4efc421fc14dbb
Pavuluri Yoganand <yoganand8944@gmail.com>
2026-07-18 23:42:47 +0530

    Fix import statements in inbuiltsort.java

--- sorting/inbuiltsort.java
+++ sorting/inbuiltsort.java
@@ -1,5 +1,6 @@
 import java.util.*;
 //import java.utils.Arrays;
+//import java.utils.Collections;
 public class inbuiltsort{
     public static void printArr(int arr[]) {
         for (int i = 0; i < arr.length; i++) {
