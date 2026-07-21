b690d4ddd57c4388eb575de81b8b05658b8086fc
Pavuluri Yoganand <yoganand8944@gmail.com>
2026-07-21 21:08:47 +0530

    Fix typo in bubble sort comment

--- sorting/allsort.java
+++ sorting/allsort.java
@@ -9,7 +9,7 @@ public class allsort{
   public static void bubble(int arr[]){
       for(int i=0;i<arr.length;i++){
           for(int j=0;j<arr.length-1-i;j++){
-              if(arr[j]>arr[j+1]){// for decending order change here 
+              if(arr[j]>arr[j+1]){// for descending order change here
                   int temp=arr[j];
                   arr[j]=arr[j+1];
                   arr[j+1]=temp;
